ee2856b5b0ba7d8a59963fd355e1c23f81ba0f01
cwang22 <wcg0304@sina.com>
2015-09-07 10:45:19 +0800

    update header to match parent theme

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -8,6 +9,7 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
+	
 	<?php wp_head(); ?>
 </head>
 
