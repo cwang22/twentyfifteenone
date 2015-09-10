8805341f483b7578d1c07ad37fc75b67680c0266
cwang22 <wcg0304@sina.com>
2015-09-10 18:31:14 +1000

    add Weibo icon to social links menu

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+    wp_enqueue_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css', array(), '4.0.3' );
 }
 ?>
+    
