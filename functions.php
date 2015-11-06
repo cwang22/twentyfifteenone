815d1b99cb8fae10661864f6a551a23b9aca5917
cwang22 <wcg0304@sina.com>
2015-11-07 10:50:05 +1100

    automatically add prettify class to <pre> tags display all line numbers

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'bootstrap-collapse', get_stylesheet_directory_uri().'/css/bootstrap.css' );
@@ -7,4 +6,10 @@ function theme_enqueue_styles() {
     wp_enqueue_script( 'run_prettify-js', '//google-code-prettify.googlecode.com/svn/loader/run_prettify.js', array(), '20151107', true );
     wp_enqueue_script( 'bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js', array('jquery'), '3.3.4', true);
 }
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function theme_add_custom_table_class( $content ) {
+    return str_replace( '<pre>', '<pre class="prettyprint linenums">', $content );
+}
+add_filter( 'the_content', 'eden_add_custom_table_class' );
 ?>
