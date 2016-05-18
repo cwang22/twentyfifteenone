e9599e84f9f14009e290bebd6c312cba2b8ec579
C. Wang <wcgdream@gmail.com>
2016-05-18 04:43:55 +0000

    update prettify url

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'bootstrap-collapse', get_stylesheet_directory_uri().'/css/bootstrap.css' );
     wp_enqueue_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css', array(), '4.0.3' );
-    wp_enqueue_script( 'run_prettify-js', '//google-code-prettify.googlecode.com/svn/loader/run_prettify.js', array(), '20151107', true );
+    wp_enqueue_script( 'run_prettify-js', '//cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js', array(), '20160518', true );
     wp_enqueue_script( 'bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js', array('jquery'), '3.3.4', true);
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
